39679b65db367159d956f9c7714274163e791d0b
jetjokers24895 <jetjokers24895@gmail.com>
2017-10-16 19:33:42 +0700

    fix

--- getrw_tiki/search_tiki.py
+++ getrw_tiki/search_tiki.py
@@ -7,7 +7,7 @@ import os
 project_dir = os.path.dirname(os.path.abspath(__file__))
 #project_dir = project_dir.replace('\\','/')
 #phantom_linuxdir = project_dir + '/phantom/linux/bin/phantomjs'
-phantom_linuxdir= '/app/getrw_tiki/phantom/linux/bin/'
+phantom_linuxdir= '/app/getrw_tiki/phantom/linux/bin/phantomjs'
 phantom_windir = project_dir + '/phantom/windows/bin/phantomjs'
 print phantom_linuxdir
 client = webdriver.PhantomJS(phantom_linuxdir) ### crawler js
